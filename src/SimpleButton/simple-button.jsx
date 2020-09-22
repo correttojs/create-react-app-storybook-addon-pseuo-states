2692b061e7c98dd023b571c6e6b61d3ccd276044
Fabio Benedetti <fabio.benedetti@7-tv.de>
2020-09-22 13:06:33 +0200

    sample with styled components

--- src/SimpleButton/simple-button.jsx
+++ src/SimpleButton/simple-button.jsx
@@ -1,21 +1,40 @@
 import * as React from 'react';
-import style from './simple-button.module.scss';
+// import style from './simple-button.module.scss';
 import classNames from 'classnames';
+import styled from 'styled-components'
+const ButtonStyle = styled.button`
+   background: violet;
+
+  &:hover {
+    background: green;
+  };
+  &:focus {
+    background: blue;
+    &:hover {
+      background: #61DAFB;
+    }
+  };
+  &:active {
+    background: red;
+  }
+`;
+
+
 
 export const SimpleButton = (props) => {
     return (
-        <button
+        <ButtonStyle
             ref={props.innerRef}
-            style={props.style}
+            // style={props.style}
             onClick={props.onClick ? props.onClick : () => null}
             disabled={props.disabled}
-            className={classNames(
-                style.Button,
-                {[style.big]: props.isBig},
-                {[style.rounded]: props.rounded}
-            )}
+            // className={classNames(
+            //     style.Button,
+            //     {[style.big]: props.isBig},
+            //     {[style.rounded]: props.rounded}
+            // )}
         >
             {props.label}
-        </button>
+        </ButtonStyle>
     );
 };
--- src/stories/1-Button.stories.js
+++ src/stories/1-Button.stories.js
@@ -1,27 +1,31 @@
-import React from 'react';
-import {AttributesStatesDefault, PseudoStatesDefault, withPseudo} from "@ergosign/storybook-addon-pseudo-states-react";
-import {SimpleButton} from "../SimpleButton/simple-button";
-import {SimpleButton__withoutCssModules} from "../SimpleButtonWithoutCssModules/simple-button";
-import {boolean} from "@storybook/addon-knobs";
+import React from "react";
+import {
+  AttributesStatesDefault,
+  PseudoStatesDefault,
+  withPseudo,
+} from "@ergosign/storybook-addon-pseudo-states-react";
+import { SimpleButton } from "../SimpleButton/simple-button";
+import { SimpleButton__withoutCssModules } from "../SimpleButtonWithoutCssModules/simple-button";
+import { boolean } from "@storybook/addon-knobs";
 
 export default {
-    title: 'Button',
-    decorators: [
-        withPseudo
-    ],
-    parameters: {
-        withPseudo: {
-            pseudos: [...PseudoStatesDefault, 'focus & hover'],
-            attributes: [...AttributesStatesDefault, {name:'isBig', value: 'really big'}],
-        }
-    }
+  title: "Button",
+  decorators: [withPseudo],
+  parameters: {
+    withPseudo: {
+      pseudos: [...PseudoStatesDefault, "focus & hover"],
+      // attributes: [...AttributesStatesDefault, {name:'isBig', value: 'really big'}],
+    },
+  },
 };
 
-export const simpleButton = () => <SimpleButton label={'ButtonLabel'} rounded={boolean('rounded', false)}/>;
+export const simpleButton = () => (
+  <SimpleButton label={"ButtonLabel"} rounded={boolean("rounded", false)} />
+);
 
-
-
-export const simpleButton__withoutCssModules = () => <SimpleButton__withoutCssModules label={'ButtonLabel'}/>;
+export const simpleButton__withoutCssModules = () => (
+  <SimpleButton__withoutCssModules label={"ButtonLabel"} />
+);
 
 // export const text = () => <Button onClick={action('clicked')}>Hello Button</Button>;
 //
